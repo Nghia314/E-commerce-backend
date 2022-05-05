675907e37647b34bb632922a01ba4f2f7ca48941
Nghia Le <davidle314@hotmail.com>
2022-05-04 20:49:16 -0700

    updating product model with rule and fields

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+   id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     autoIncrement: true
+   },
+   product_name: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   price: { 
+     type: DataTypes.DECIMAL,
+     allowNull: false,
+     validate: {
+       isDecimal: true
+     }
+     },
+     stocks: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       defaultValue: 10,
+       validate: {
+         isNumeric: true
+       }
+     },
+     category_id: {
+       type: DataTypes.INTEGER,
+       references: {
+         model: 'catogry',
+         key: 'id'
+       }
+     }
   },
+
+
   {
     sequelize,
     timestamps: false,
@@ -18,6 +51,6 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
-);
+  );
 
 module.exports = Product;
